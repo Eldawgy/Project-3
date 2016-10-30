96f143bb441974cad1e1a9cf49caed00f79c754c
MikeyGBG <Michael.Garthe@hotmail.com>
2016-10-30 22:07:26 +1000

    Update num_2_angle.m

--- current/num_2_angle.m
+++ current/num_2_angle.m
@@ -3,7 +3,7 @@ function angles = num_2_angle(ids, nums)
 %NOTE: the numbers passed must be ordered the same as ids
 for i = 1:length(ids)
     if (ids(i) == 3)
-        angles(i) = round((nums(i) - 512)/(3.41));
+        angles(i) = round((nums(i) - 512)/(3.41));  %3.41 = (1023/300)->numbers/degrees
     elseif (ids(i) == 2) 
         angles(i) = round((nums(i) - 205) / (3.41));
     elseif (ids(i) == 1)
