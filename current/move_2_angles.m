158c8c8832a5b1a851bb34299bbf506eb79e5ff6
MikeyGBG <Michael.Garthe@hotmail.com>
2016-10-30 22:05:06 +1000

    Update move_2_angles.m

--- current/move_2_angles.m
+++ current/move_2_angles.m
@@ -2,7 +2,6 @@ function move_2_angles(ids, angles)
     %Get the current position of the motors
     posNum = get_pos();
     %find the current angle of the motors
-    angles(3) = angles(3);
     currAngle = num_2_angle(ids, posNum);
     
     disp('Moving to angles'); angles(:)
